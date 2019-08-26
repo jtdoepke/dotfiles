c81a062dbf96277be3b67689b6d96ff80f7bd79f
Jaye Doepke <jdoepke@mintel.com>
2019-08-26 09:10:14 -0500

    Add thefuck config

---
+++ private_dot_config/private_thefuck/private_settings.py
@@ -0,0 +1,18 @@
+# The Fuck settings file
+#
+# The rules are defined as in the example bellow:
+#
+# rules = ['cd_parent', 'git_push', 'python_command', 'sudo']
+#
+# The default values are as follows. Uncomment and change to fit your needs.
+# See https://github.com/nvbn/thefuck#settings for more information.
+#
+
+# rules = [<object object at 0x7f632cae7350>]
+# exclude_rules = []
+# wait_command = 3
+# require_confirmation = True
+# no_colors = False
+# debug = False
+# priority = {}
+# env = {'LC_ALL': 'C', 'LANG': 'C', 'GIT_TRACE': '1'}
